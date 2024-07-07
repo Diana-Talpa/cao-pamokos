e81b3d5042beb4b7afdb7c17aff3a683eab91792
Diana.Talpa <dianajemeljanovaite@gmail.com>
2024-07-07 16:56:51 +0300

    js darbas papildomas

---
+++ papildomas js darbas/script.js
@@ -0,0 +1,106 @@
+
+
+// let passw = 'bangladesas#'
+
+// if(passw.lenght < 16) {
+//     alert('Slaptazodis per trumpas')
+// } else if(passw.lenght >= 20) {
+//     alert('Slaptazodis yra tinkamas')
+// } 
+
+
+
+
+// const simbolsNeeded = ["#"];
+// const password = "Patrick#";
+// let specialCharacterCheck = false;
+
+// const special = (c) => {
+//   for (i = 0; i < simbolsNeeded.length; i++) {
+//     if (c === simbolsNeeded[i]) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
+
+// for (i = 0; i < password.length; i++) {
+//   if (special(password[i])) {
+//     specialCharacterCheck = true;
+//   }
+// }
+// console.log(specialCharacterCheck);
+
+// if(password.length < 16){
+//     console.log('Slaptazodis per trumpas')
+// } else if(password.length > 20) {
+//     console.log ('Slaptazodis tinkamas')
+// } else if(password.length < 20) {
+//     console.log ('Slaptazodis tinkamas , bet rekomenduojame naudoti bent 20 simboliu')
+// }
+
+
+
+
+// const amzius = 6
+
+// if(amzius <= 0) {
+// console.log('Yvestas amzius per mazas')
+// }
+// else if(amzius < 7 ) {
+//     console.log ('I mokykla neina')
+// }
+//  else if( amzius <= 10) {
+//     console.log('Eina i pradine klase')
+// }
+//  else if( amzius <= 14) {
+//     console.log('Eina i pagrindine')
+// }
+//  else if( amzius <= 18) {
+//     console.log('Eina i gimnazija')
+// }
+//  else if(amzius >= 19) {
+//     console.log('Mokykla baigeS')
+// }
+//  else if  (amzius > 120) {
+//     console.log('Amzius yra per didelis')
+// }
+
+
+//  else if(amzius = 10) {
+//     console.log('Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas.')
+// }
+//  else if(amzius = 14) {
+//     console.log('Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas.')
+// }
+//  else if(amzius = 18) {
+//     console.log('Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs.')
+// }
+
+
+
+const amzius = 19;
+
+if (amzius <= 0) {
+    console.log('Yvestas amzius per mazas');
+} else if (amzius == 6) {
+    console.log('Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas.');
+} else if (amzius < 7) {
+    console.log('I mokykla neina');
+} else if (amzius < 10) {
+    console.log('Eina i pradine klase');
+} else if (amzius == 10) {
+    console.log('Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas.');
+} else if (amzius < 14) {
+    console.log('Eina i pagrindine');
+} else if (amzius == 14) {
+    console.log('Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas.');
+} else if (amzius < 18) {
+    console.log('Eina i gimnazija');
+} else if (amzius == 18) {
+    console.log('Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs.');
+} else if (amzius > 120) {
+    console.log('Amzius yra per didelis');
+} else if (amzius >= 19) {
+    console.log('Mokykla baigęs');
+}
