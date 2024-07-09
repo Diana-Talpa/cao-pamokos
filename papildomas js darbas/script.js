c6b24d59d2b397178aa2aa06ec254f1a4859ed5c
Diana.Talpa <dianajemeljanovaite@gmail.com>
2024-07-09 15:47:04 +0300

    jup

--- papildomas js darbas/script.js
+++ papildomas js darbas/script.js
@@ -1,115 +1,131 @@
 
 
-let passw = 'bangladesas#'
+let password = 'fgdfgdfdfggvjhvhvgg#'
+let passwordLength = password.trim( ).length
 
-if(passw.lenght < 16) {
-    alert('Slaptazodis per trumpas')
-} else if(passw.lenght >= 20) {
-    alert('Slaptazodis yra tinkamas')
-} 
-
-
-
-
-const simbolsNeeded = ["#"];
-const password = "Patrick#";
-let specialCharacterCheck = false;
-
-
-//reikiaq naudoti include o ne sita visa pieva 
-const special = (c) => {
-  for (i = 0; i < simbolsNeeded.length; i++) {
-    if (c === simbolsNeeded[i]) {
-      return true;
-    }
-  }
-  return false;
-}
-
-for (i = 0; i < password.length; i++) {
-  if (special(password[i])) {
-    specialCharacterCheck = true;
-  }
-}
-console.log(specialCharacterCheck);
-
-if(password.length < 16){
-    console.log('Slaptazodis per trumpas')
-} else if(password.length > 21) {
-    console.log ('Slaptazodis tinkamas')
-} else if(password.length < 21) {
-    console.log ('Slaptazodis tinkamas , bet rekomenduojame naudoti bent 20 simboliu')
+if (!password.includes('#')) {
+  console.log('Nėra grotažymių :(')
+} else if (passwordLength < 16) {
+  console.log('Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo.')
+} else if (passwordLength < 21) {
+  console.log('Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo.')
+} else {
+  console.log('Slaptažodis tinkamas')
 }
 
 
 
+let age = Number('asdasd')
 
-// const amzius = 6
-
-// if(amzius <= 0) {
-// console.log('Yvestas amzius per mazas')
-// }
-// else if(amzius < 7 ) {
-//     console.log ('I mokykla neina')
-// }
-//  else if( amzius <= 10) {
-//     console.log('Eina i pradine klase')
-// }
-//  else if( amzius <= 14) {
-//     console.log('Eina i pagrindine')
-// }
-//  else if( amzius <= 18) {
-//     console.log('Eina i gimnazija')
-// }
-//  else if(amzius >= 19) {
-//     console.log('Mokykla baigeS')
-// }
-//  else if  (amzius > 120) {
-//     console.log('Amzius yra per didelis')
-// }
-
-
-//  else if(amzius = 10) {
-//     console.log('Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas.')
-// }
-//  else if(amzius = 14) {
-//     console.log('Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas.')
-// }
-//  else if(amzius = 18) {
-//     console.log('Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs.')
-// }
-
-
-
-const amzius = 19;
-
-if (amzius <= 0) {
-    console.log('Yvestas amzius per mazas');
-} else if (amzius == 6) {
-    console.log('Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas.');
-} else if (amzius < 7) {
-    console.log('I mokykla neina');
-} else if (amzius < 10) {
-    console.log('Eina i pradine klase');
-} else if (amzius == 10) {
-    console.log('Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas.');
-} else if (amzius < 15) {
-    console.log('Eina i pagrindine');
-} else if (amzius == 14) {
-    console.log('Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas.');
-} else if (amzius < 19) {
-    console.log('Eina i gimnazija');
-} else if (amzius == 18) {
-    console.log('Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs.');
-} else if (amzius > 120) {
-    console.log('Amzius yra per didelis');
-} else if (amzius >= 19) {
-    console.log('Mokykla baigęs');
+if (isNaN(age)) {
+  console.log('Netinkamai nurodytas amžius, amžius privalo būti skaičius.')
+} else if (age < 0) {
+  console.log('Amžius privalo būti teigiamas skaičius')
+} else if (age < 6) {
+  console.log('Neina mokykla')
+} else if (age < 7) {
+  console.log('Į mokyklą tikriausiai neina, tačiau gali būti ir pirmokas.')
+} else if (age < 10) {
+  console.log('Eina į pradinę klasę')
+} else if (age < 11) {
+  console.log('Tikriausiai mokosi pradinėje, tačiau gali būti ir penktokas.')
+} else if (age < 14) {
+  console.log('Eina į pagrindinę')
+} else if (age < 15) {add
+  console.log('Tikriausiai mokosi pagrindinėje, tačiau gali būti ir devintokas.')
+} else if (age < 18) {
+  console.log('Eina į gimnaziją')
+} else if (age < 19) {
+  console.log('Tikriausiai mokosi gimnazijoje, tačiau mokyklą gali būti ir baigęs.')
+} else if (age < 120) {
+  console.log('Mokykla baigė')
+} else {
+  console.log('Amžius negali būti didesnis už 120 metų')
 }
 
-//reikejo naudoti 120 pabaigoje beveik typo > 120 o poto else jau butu negalimas nes perdidelis
 
+//////Aciu Aldai uz koda! :)
 
+const gameFirstLvl = 1
+const gameSecondLvl = 2
+const gameThirdLvl = 3
 
+let userAnswer1, userAnswer2, userAnswer3
 
+//// 1 lygis
+if (gameFirstLvl == 1) {
+   userAnswer1 = +prompt('Kiek yra 1 + 1?')
+   console.log(userAnswer1)
 
+   userAnswer2 = +prompt('Kiek yra 1 + 2?')
+   console.log(userAnswer2)
+
+   if (userAnswer1 === 2 && userAnswer2 === 3) {
+      console.log('Teisingai! Atsakei į abu klausimus!')
+      console.log('Patekai i kita lygi!')
+   } else if (userAnswer1 === 2 && userAnswer2 !== 3) {
+      console.log('Patekai i kita lygi: pirmas atsakymas buvo teisingas, o antras buvo neteisingas.')
+   } else if (userAnswer1 !== 2 && userAnswer2 === 3) {
+      console.log('Patekai i kita lygi: antras atsakymas buvo teisingas, o pirmas buvo neteisingas.')
+   } else {
+      console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
+   }
+} else {
+   console.log('Klaida. Nepavyko patekti i kita lygi.')
+}
+
+//// 2 lygis
+if (gameSecondLvl == 2) {
+   userAnswer2 = +prompt('Kiek yra 2 + 2?')
+   console.log(userAnswer2)
+
+   userAnswer3 = +prompt('Kiek yra 2 + 3?')
+   console.log(userAnswer3)
+
+   if (userAnswer2 === 4 && userAnswer3 === 5) {
+      console.log('Teisingai! Atsakei į abu klausimus!')
+      console.log('Patekai i kita lygi!')
+   } else if (userAnswer2 !== 4 && userAnswer3 === 5) {
+      console.log('Nepatekai i kita lygi: pirmas atsakymas buvo neteisingas, o antras buvo teisingas.')
+   } else if (userAnswer2 === 4 && userAnswer3 !== 5) {
+      console.log('Nepatekai i kita lygi: pirmas atsakymas buvo teisingas, o antras buvo neteisingas.')
+   } else {
+      console.log('Nepatekai i kita lygi: abu atsakymai buvo neteisingi.')
+   }
+} else {
+   console.log('Klaida. Nepavyko patekti i kita lygi.')
+}
+
+//// 3 lygis
+if (gameThirdLvl == 3) {
+   userAnswer1 = +prompt('Kiek yra 3 + 3?')
+   console.log(userAnswer1)
+
+   userAnswer2 = +prompt('Kiek yra 3 + 4?')
+   console.log(userAnswer2)
+
+   userAnswer3 = +prompt('Kiek yra 3 + 5?')
+   console.log(userAnswer3)
+
+   if (userAnswer1 === 6 && userAnswer2 === 7 && userAnswer3 === 8) {
+      console.log('Teisingai! Atsakei į visus klausimus!')
+      console.log('Laimejote!')
+   } else if (userAnswer1 === 6 && userAnswer2 === 7 && userAnswer3 !== 8) {
+      console.log('Patekai i kita lygi: pirmas ir antras atsakymai buvo teisingi, o trecias buvo neteisingas.')
+   } else if (userAnswer1 === 6 && userAnswer2 !== 7 && userAnswer3 === 8) {
+      console.log('Patekai i kita lygi: pirmas ir trecias atsakymai buvo teisingi, o antras buvo neteisingas.')
+   } else if (userAnswer1 !== 6 && userAnswer2 === 7 && userAnswer3 === 8) {
+      console.log('Patekai i kita lygi: antras ir trecias atsakymai buvo teisingi, o pirmas buvo neteisingas.')
+   } else if (userAnswer1 === 6 && userAnswer2 !== 7 && userAnswer3 !== 8) {
+      console.log('Nepatekai i kita lygi: pirmas atsakymas buvo teisingas, o kiti buvo neteisingi.')
+   } else if (userAnswer1 !== 6 && userAnswer2 === 7 && userAnswer3 !== 8) {
+      console.log('Nepatekai i kita lygi: antras atsakymas buvo teisingas, o kiti buvo neteisingi.')
+   } else if (userAnswer1 !== 6 && userAnswer2 !== 7 && userAnswer3 === 8) {
+      console.log('Nepatekai i kita lygi: trecias atsakymas buvo teisingas, o kiti buvo neteisingi.')
+   } else {
+      console.log('Nepatekai i kita lygi: visi atsakymai buvo neteisingi.')
+   }
+} else {
+   console.log('Klaida. Nepavyko patekti i kita lygi.')
+}
+ //////Aciu Aldai uz koda! :)
