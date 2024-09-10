7ce8afe8e1bcc75d0778edfd05c207cb22613572
Diana.Talpa <dianajemeljanovaite@gmail.com>
2024-09-10 09:33:34 +0300

    comits for weekend 09 mnt 07.09

---
+++ react/react-my-first-react-app/src/components/PageContent.js
@@ -0,0 +1,15 @@
+import ContentSection from "./ContentSection"
+
+function PageContent() {
+    return (
+        <main>
+            <h1>PAvadinimas</h1>
+
+            <ContentSection/>
+            <ContentSection/>
+            <ContentSection/>
+        </main>
+    )
+}
+
+export default PageContent
---
+++ react/react-my-first-react-app/src/components/PageFooter.js
@@ -0,0 +1,17 @@
+import FooterSection from "./FooterSection"
+
+function PageFooter () {
+    return (
+        <footer>
+            <div>
+
+            
+            <footerSEction/>
+            <footerSEction/>
+            <footerSEction/>
+            </div>
+        </footer>
+    )
+}
+
+export default PageFooter
